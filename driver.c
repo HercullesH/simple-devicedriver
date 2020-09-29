bd1adcae6de9ec4406cdca12ea1f3d564a67e341
Herculles Hendrius <herculles.hendriuss@gmail.com>
2020-09-29 00:29:36 -0300

    melhorias

--- driver.c
+++ driver.c
@@ -1,12 +1,12 @@
 #include <linux/init.h>
 #include <linux/module.h>
 MODULE_LICENSE("GPL");
-static int hello_init(void){
-    printk(KERN_ALERT "Hello, world\n");
+static int load(void){
+    printk(KERN_ALERT "Carregou o módulo no kernel\n");
     return 0;
 }
-static void hello_exit(void){
-    printk(KERN_ALERT "Goodbye, cruel world\n");
+static void remove(void){
+    printk(KERN_ALERT "Foi removido do kernel\n");
 }
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(load);
+module_exit(remove);
